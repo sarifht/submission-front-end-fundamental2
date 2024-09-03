ba08a8eb15019e957b2183d387507923cd1d782b
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-09-03 20:55:48 +0700

    Update kodingan webpack.prod.js agar menghasilkan output public

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,13 @@
 const { merge } = require("webpack-merge");
+const path = require("path"); // Tambahkan ini untuk mengakses 'path' module
 const common = require("./webpack.common");
 
 module.exports = merge(common, {
   mode: "production",
+  output: {
+    path: path.resolve(__dirname, "public"), // Tentukan folder 'public' sebagai direktori output
+    filename: "bundle.js", // Nama file output, bisa diubah sesuai kebutuhan
+  },
   module: {
     rules: [
       {
